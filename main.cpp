b2c68833d4c8d1f1636e78f14ddad52547584e72
manasij7479 <manasij7479@gmail.com>
2014-09-20 05:37:35 +0530

    remove everything

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
-#include <thread>
-#include "mess.hpp"
-#include "comm.hpp"
+
 int main(int argc,char** argv)
 {
-	std::string user,ip,extip;;
-	 
-	switch(argc)
-	{
-		case 4:extip=argv[3];
-		case 3:user=argv[1];ip=argv[2];break;
-		default: std::cerr<<"Invalid Options.";return -1;
-	}
-	
-	Mess::Peer peer(user,ip);
-	if(extip!="")
-		peer.sync(ip);
-	
-	return peer(); 
+
 }
